a373a5f313dac24ecda39e353dcb9c57cc570ad5
Jake <jacobtlanghel@gmail.com>
2023-06-03 00:26:33 -0400

    add limiter

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10min
+  max: 100,
+});
+
+app.use(limiter);
+app.set("trust proxy", 1);
 
 // Enable cors
 app.use(cors());
